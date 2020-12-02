a87e7543856607d3aa7b260e40f9e8335a63dee2
ThiagoGMF <thiago.guilherme20@hotmail.com>
2020-12-02 19:52:38 -0300

    Adicionando algoritmo de compressão de cordenadas

---
+++ DataStructures/CoordinateCompression.cpp
@@ -0,0 +1,27 @@
+class CoordinateCompression {
+
+	private:
+
+		vector<int> a;
+
+	public:
+
+		CoordinateCompression(const vector<int> &v){
+			a = v;
+			sort(a.begin(),a.end());
+			a.resize(unique(a.begin(),a.end()) - a.begin());
+		}
+
+		int getL(int x){
+			return lower_bound(a.begin(),a.end(),x) - a.begin();
+		}
+
+		int getR(int x){
+			return upper_bound(a.begin(),a.end(),x) - a.begin();
+		}
+
+		int size() const {
+			return (int) a.size();
+		}
+
+};
